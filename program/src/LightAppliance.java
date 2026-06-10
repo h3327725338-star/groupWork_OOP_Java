4f8b5e0554eee3acd045de28aeff5184620345c7
HU SHUNBO <h3327725338@gmail.com>
2026-06-10 14:13:20 +0800

    Completed the writing of the LightAppliance.java file.
    Created a light appliance object that can store its name, type, power rating, usage hours, energy consumed, and bulb type. It can also modify data, read data, check invalid input, calculate energy consumption, and display complete information.

--- program/src/LightAppliance.java
+++ program/src/LightAppliance.java
@@ -1,3 +1,39 @@
-public class LightAppliance {
-    
+public class LightAppliance extends Appliance {
+    private String bulbType;
+
+    public LightAppliance(String name, double powerRating, String lightBulbType) {
+        super(name, "Light", powerRating);
+        setBulbType(lightBulbType);
+    }
+
+    public String getBulbType() {
+        return bulbType;
+    }
+
+    public void setBulbType(String lightBulbType) {
+        if (lightBulbType == null) {
+            bulbType = "General Bulb";
+        }
+        else if (lightBulbType.equals("")) {
+            bulbType = "General Bulb";
+        }
+        else {
+            bulbType = lightBulbType;
+        }
+    }
+
+    public double calculateEnergyConsumption() {
+        double energy = (getPowerRating() * getUsageHours()) / 1000;
+        setEnergyConsumed(energy);
+        return energy;
+    }
+
+    public void displayInfo() {
+        System.out.println("Appliance Name: " + getName());
+        System.out.println("Type: " + getType());
+        System.out.println("Bulb Type: " + bulbType);
+        System.out.println("Power Rating: " + getPowerRating() + " watts");
+        System.out.println("Usage Hours: " + getUsageHours() + " hours");
+        System.out.println("Energy Consumed: " + getEnergyConsumed() + " kWh");
+    }
 }
